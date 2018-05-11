b1784acdd6535b101eaa8bb195d2a37250e1d314
Gabriel Esu <mgabrielesu@gmail.com>
2018-05-11 08:57:12 +0100

    included loaders and configured css extract

--- src/components/Sample.js
+++ src/components/Sample.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Logo from '../images/Logo.svg';
 
 
 
 const Sample = () => {
     return(
-        <div>
-            <h1>This Works!</h1>
+        <div className="intro">
+            <img src={Logo} className="logo" alt=""/>
+            <h2>The Perfect Starter React Boilerplate for Newbies (with Webpack 4)</h2>
+            <p>With Hot Module Reload, Babel-loader, Sass-loader, Autoprefixer, Image Bundling ...</p>
         </div>
     )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,70 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
+const autoprefixer = require('autoprefixer');
+
+
+module.exports = (env, argv) => {
+
+    const devMode = argv.mode !== 'production';
+
+    return {
+        entry: './src/index.js',
+        output: {
+            path: path.join(__dirname, '/dist'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader'
+                    }
+                },
+                {
+                    test: /\.s?[ac]ss$/,
+                    use:[
+                        devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        {
+                            loader: "postcss-loader",
+                            options: {
+                                autoprefixer: {
+                                    browsers: ["last 2 versions"]
+                                },
+                                plugins: () => [
+                                    autoprefixer
+                                ]
+                            },
+                        },
+                        'sass-loader'
+                    ]
+                },
+                {
+                    test: /\.(png|jpg|gif|svg)$/,
+                    use:{
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'images/',
+                            useRelativePath: true
+                        }
+                    }
                 }
-            },
-            {
-                test: /.(s*)css$/,
-                use:['style-loader','css-loader', 'sass-loader']
-            }
+            ]
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: devMode ? '[name].css' : '[name].[hash].css',
+                chunkFilename: devMode ? '[id].css' : '[id].[hash].css'
+            }),
+            new HtmlWebpackPlugin({template: './src/index.html'})
+
         ]
-    },
-    plugins: [new HtmlWebpackPlugin({template: './src/index.html'})]
+    }
 }
